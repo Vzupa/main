6f58e08824a488ee77d11c65791b7f3942f923b8
Vito <99677852+Vzupa@users.noreply.github.com>
2025-07-10 11:15:40 +0200

    Update tmux_autostart.zsh

--- static/tmux_autostart.zsh
+++ static/tmux_autostart.zsh
@@ -2,3 +2,5 @@
 if which tmux >/dev/null && [ -z "$TMUX" ]; then
   tmux attach-session -t default || tmux new-session -s default
 fi
+
+source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
