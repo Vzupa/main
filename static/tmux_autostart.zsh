3ffd74fe683de60757ec20d619570ca96b0d4187
Vzupa <vzupa@pm.me>
2025-07-12 09:35:35 -0400

    Added zsh suggestions to the tmuxconf static

--- static/tmux_autostart.zsh
+++ static/tmux_autostart.zsh
@@ -3,4 +3,5 @@ if which tmux >/dev/null && [ -z "$TMUX" ]; then
   tmux attach-session -t default || tmux new-session -s default
 fi
 
+source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
